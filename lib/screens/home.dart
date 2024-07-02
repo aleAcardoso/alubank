a995ca64971fd01d19abde38dfc7f9e8ce11fe62
Alexandre Antonelli Cardoso <aacds@msn.com>
2024-07-02 10:10:38 -0300

    Implementando a nova seção (#5)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,5 @@
-import 'package:alubank/components/box_card.dart';
 import 'package:alubank/components/sections/header.dart';
+import 'package:alubank/components/sections/recent_activity.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -11,7 +11,7 @@ class Home extends StatelessWidget {
       body: Column(
         children: [
           Header(),
-          BoxCard(boxContent: Text('Olá mundo!'))
+          RecentActivity()
         ],
       ),
     );
