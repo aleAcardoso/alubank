a2b6698717eb10c82fe699fff2119e3ad09499bd
Alexandre Antonelli Cardoso <aacds@msn.com>
2024-07-02 11:45:19 -0300

    Feature/course one (#7)
    * Começando o projeto (#1)
    
    * Estilizando Widgets (#2)
    
    * Criando novos componentes (#3)
    
    * Implementando temas (#4)
    
    * Implementando a nova seção (#5)
    
    * Estilizações finais: ações e tipografia (#6)

---
+++ lib/components/sections/header.dart
@@ -0,0 +1,47 @@
+import 'package:alubank/themes/theme_colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: ThemeColors.headerGradient,
+          ),
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(15.0))),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text.rich(TextSpan(text: '\$', children: [
+                  TextSpan(
+                    text: '1000.00',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  )
+                ])),
+                const Text(
+                  'Balanço disponível',
+                )
+              ],
+            ),
+            const Icon(
+              Icons.account_circle,
+              size: 42,
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
