4b0441940e93f8dafba1303598f4bdf2e3acf984
xandelol <aacds@msn.com>
2024-07-02 11:42:08 -0300

    Estilizações finais: ações e tipografia

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,17 +1,28 @@
+import 'package:alubank/components/sections/account_actions.dart';
+import 'package:alubank/components/sections/account_points.dart';
 import 'package:alubank/components/sections/header.dart';
 import 'package:alubank/components/sections/recent_activity.dart';
+import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: ThemeColors.headerGradient[0]
+      ),
+    );
+    return Scaffold(
+      body: ListView(
+        children: const [
           Header(),
-          RecentActivity()
+          RecentActivity(),
+          AccountActions(),
+          AccountPoints()
         ],
       ),
     );
